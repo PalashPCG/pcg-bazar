16b549ba8a9d1b807f78c98ed3db9063ecea64e6
PalashPCG <engr.pcg@gmail.com>
2022-11-23 01:35:03 +0600

    pcg-bazar

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">PCG-Bazar</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
